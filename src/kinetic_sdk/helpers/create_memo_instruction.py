55d02eb4b23daaae5eaf0e8855cc4a4a88043b39
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2022-12-28 16:36:57 +0700

    fix: property encode Kin memo  (#44)

--- src/kinetic_sdk/helpers/create_memo_instruction.py
+++ src/kinetic_sdk/helpers/create_memo_instruction.py
@@ -9,7 +9,7 @@ from kinetic_sdk.models import KinMemo, TransactionType
 
 def create_memo_instruction(index: int, tx_type: TransactionType) -> TransactionInstruction:
     memo: bytearray = KinMemo.new(1, tx_type, index).val
-    data: bytes = base64.b64decode(memo)
+    data: bytes = base64.b64encode(memo)
 
     return TransactionInstruction(
         data=data, keys=[], program_id=PublicKey("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo")
